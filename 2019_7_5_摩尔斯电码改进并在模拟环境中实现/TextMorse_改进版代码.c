56cff772208abaa3679f8d1e9b19701043f52a20
Developer-Isaac-Xu <47655672+Developer-Isaac-Xu@users.noreply.github.com>
2019-07-05 10:13:28 +0800

    Add files via upload

---
+++ 2019_7_5_摩尔斯电码改进并在模拟环境中实现/TextMorse_改进版代码.c
@@ -0,0 +1,55 @@
+int led = 5;
+
+void dash()
+{
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(250);
+}
+void spa()
+{
+  delay(3000);
+}
+
+void dot()
+{
+  digitalWrite(led,HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(250);
+}
+
+char MORSE[][5] = {
+  {'*', '-'},{'-', '*', '*', '*'},{'-', '*', '-', '*'},{'-', '*', '*'},{'*'},{'*', '*', '-', '*'},{'-', '-', '*'},{'*', '*', '*', '*'},{'*', '*'},{'*', '-', '-', '-'},{'-', '*', '-'},{'*', '-', '*', '*'},{'-', '-'},{'-', '*'},{'-', '-', '-'},{'*', '-', '-', '*'},{'-', '-', '*', '-'},{'*', '-', '*'},{'*', '*', '*'},{'-'},{'*', '*', '-'},{'*', '*', '*', '-'},{'*', '-', '-'},{'-', '*', '*', '-'},{'-', '*', '-', '-'},{'-', '-', '*', '*'}
+};
+void setup() 
+{
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+int comechar = 0;
+void loop() 
+{
+  String morse = "";
+  char sign;
+  if (Serial.available() > 0)
+  {
+    comechar = Serial.read() - 97;
+    if (comechar >= 0 && comechar <= 25)
+    {
+      morse = MORSE[comechar];
+      morse += ' ';
+      for (int i = 0;i < 5; i++)
+      {
+        sign = morse[i];
+        if (sign == '*')
+          dot();
+        else if (sign == '-')
+          dash();
+      }
+    }
+    else
+    spa();
+  }
+}
